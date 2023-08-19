1474c267f73c077c800e557ab8e07ceca3de30b2
PACHONBOTS <142656807+PACHONBOTS@users.noreply.github.com>
2023-08-19 02:36:34 -0600

    Update owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
-const owner = 'BrunoSobrino';
-const repo = 'TheMystic-Bot-MD';
+const owner = 'PACHON DOMINA';
+const repo = 'TheNaniBot';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   async function checkRepoUpdates() {
     try {
